656797b57292c936caf749e687a3661945cfec90
cwillhelm <connor.willhelm@gmail.com>
2019-04-20 23:02:23 -0400

    adding webhooks
    Signed-off-by: cwillhelm <connor.willhelm@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO=https://github.com/cwillhelm/docker-compose-hugo-ssl.git
+WORKING_DIRECTORY=$HOME/staging
+DOMAIN=rustedbits.com
+
+rm -rf $WORKING_DIRECTORY
+
+rsync -aqz ~/rustedbits/ ~/rustedbits_backup
+rsync -aqz $WORKING_DIRECTORY $HOME/staging_backup
+
+git clone $REPO $WOKRING_DIRECTORY
+
+#cd ~/rustedbits/
+#docker-compose down
+
+cd ~/staging
+docker-compose down
+docker-compose up -d 
+
+echo "Deployment completed successfully"
