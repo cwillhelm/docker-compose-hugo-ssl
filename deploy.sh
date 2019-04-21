05c408945cd6cc8acb44e03a300f1918d6fd6c63
cwillhelm <connor.willhelm@gmail.com>
2019-04-20 23:09:19 -0400

    testing webhooks
    Signed-off-by: cwillhelm <connor.willhelm@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,20 @@ REPO=https://github.com/cwillhelm/docker-compose-hugo-ssl.git
 WORKING_DIRECTORY=$HOME/staging
 DOMAIN=rustedbits.com
 
+# Clean working directory
 rm -rf $WORKING_DIRECTORY
 
+# Backup current version
 rsync -aqz $HOME/rustedbits/ $HOME/rustedbits_backup
 rsync -aqz $WORKING_DIRECTORY $HOME/staging_backup
 
+# Get updated version
 git clone $REPO $WOKRING_DIRECTORY
 
 #cd ~/rustedbits/
 #docker-compose down
 
+# Redeploy website
 cd ~/staging
 docker-compose down
 docker-compose up -d 
